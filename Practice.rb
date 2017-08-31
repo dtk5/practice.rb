36cf20abdf7b3328ebd57f59afbfe346fe9ac3ac
dtk5 <30960879+dtk5@users.noreply.github.com>
2017-08-31 12:46:37 -0700

    Update Practice.rb

--- Practice.rb
+++ Practice.rb
@@ -1,17 +1,17 @@
-# You are given an array of String objects, stored in a variable called
-# my_strings. You want to use this array to create a new array of
-#Integer objects, which represents the length of each string.
-#We want to store the resulting array in a variable called
-#string_lengths.
+# Loop Problem Practice
+# For each of the following problems, write the code to solve the problem. Each solution should be structured as a 
+# method that takes an array as a parameter and returns the result. You should use the Array#each method to solve each problem.
 
 class Practice
 
   def initialize()
     @string_lengths = Array.new
     @numChecker = Array.new
-
   end
 
+ # You are given an array of String objects, stored in a variable called my_strings. You want to use this array to create 
+ # a new array of Integer objects, which represents the length of each string. We want to store the resulting array in a 
+ # variable called string_lengths.
   def check_string(my_strings)
     my_strings.each do |i|
       @string_lengths << i.length
@@ -19,7 +19,11 @@ class Practice
     return "#{@string_lengths}"
   end
 
-  def cut_string(my_strings)
+
+# You are given an array of String objects, stored in a variable called my_strings. You want to create a new array which 
+# only contains the strings from the original array that have a length that is smaller than 4. We want to store the 
+# resulting array in the original my_strings variable, overwriting the original value.
+ def cut_string(my_strings)
     new_string = []
     my_strings.each do |i|
       new_string << i[0..3]
@@ -28,6 +32,9 @@ class Practice
     return "#{my_strings}"
   end
 
+  # You are given an array of Integer objects, stored in a variable called my_nums. You want to use this array 
+  #to create a new array which contains String objects, which say "even" or "odd" depending on the Integer in 
+  #the original array.
   def even_or_odd(my_nums)
     my_nums.each do |i|
       if(i%2==0)
@@ -39,6 +46,8 @@ class Practice
     return "#{@numChecker}"
   end
 
+  # You are given an array of String objects, stored in a variable called my_strings. You want to use this array to return 
+  # the String that has the longest length. (In a tie, it should return the first String with the longest length)
   def longest_string(my_strings)
     longest = ""
     my_strings.each do |i|
@@ -49,20 +58,11 @@ class Practice
     return longest
   end
 end
+
+#tests
   danny = Practice.new()
   puts danny.check_string(["danny", "Saomemdma", "da"])
   puts danny.even_or_odd([5, 2, 7,9,19,10])
   puts danny.cut_string(["danny", "Saomemdma", "daniel"])
   puts danny.longest_string(["dannydaid", "Saomemdma", "daniel"])
 
-
-# You are given an array of String objects, stored in a variable
-#called my_strings. You want to create a new array which only
-#contains the strings from the original array that have a length
-#that is smaller than 4. We want to store the resulting array in
-#the original my_strings variable, overwriting the original value.
-#
-# You are given an array of String objects, stored in a variable
-#called my_strings. You want to use this array to return the String
-# that has the longest length. (In a tie, it should return the first
-# String with the longest length)
